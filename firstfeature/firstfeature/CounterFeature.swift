9922b81750e4a334c073e00be8dd57e5d9c9c9a7
_GGDol <bohg6799@naver.com>
2026-02-03 17:23:18 +0900

    Feat: Testing your feature Upload

--- firstfeature/firstfeature/CounterFeature.swift
+++ firstfeature/firstfeature/CounterFeature.swift
@@ -6,31 +6,31 @@
 //
 
 import ComposableArchitecture
-import Foundation
+
 
 @Reducer
 struct CounterFeature {
-    
     @ObservableState
-    struct State {
+    struct State: Equatable, Sendable {
         var count = 0
         var fact: String?
         var isLoading = false
         var isTimerRunning = false
     }
     
-    enum Action {
+    enum Action: Sendable {
         case decrementButtonTapped
         case factButtonTapped
-        case factResponse(Result<String, Error>)
+        case factResponse(String)
         case incrementButtonTapped
         case timerTick
         case toggleTimerButtonTapped
     }
     
-    nonisolated enum CancelID {
-        case timer
-    }
+    nonisolated enum CancelID { case timer }
+    
+    @Dependency(\.continuousClock) var clock
+    @Dependency(\.numberFact) var numberFact
     
     var body: some Reducer<State, Action> {
         Reduce { state, action in
@@ -44,28 +44,20 @@ struct CounterFeature {
                 state.fact = nil
                 state.isLoading = true
                 return .run { [count = state.count] send in
-
-                    do {
-                        let (data, _) = try await URLSession.shared
-                            .data(from: URL(string: "https://numbersapi.com/\(count)")!)
-                        let fact = String(decoding: data, as: UTF8.self)
-                        await send(.factResponse(.success(fact)))
-                    } catch {
-                        await send(.factResponse(.failure(error)))
-                    }
+                    try await send(.factResponse(self.numberFact.fetch(count)))
                 }
                 
-            case let .factResponse(.success(fact)):
+            case let .factResponse(fact):
                 state.fact = fact
                 state.isLoading = false
                 return .none
                 
-            case .factResponse(.failure):
-                state.isLoading = false
-                state.fact = "에러가 발생했습니다. 다시 시도해주세요."
+            case .incrementButtonTapped:
+                state.count += 1
+                state.fact = nil
                 return .none
                 
-            case .incrementButtonTapped:
+            case .timerTick:
                 state.count += 1
                 state.fact = nil
                 return .none
@@ -74,8 +66,7 @@ struct CounterFeature {
                 state.isTimerRunning.toggle()
                 if state.isTimerRunning {
                     return .run { send in
-                        while true {
-                            try await Task.sleep(for: .seconds(1))
+                        for await _ in await self.clock.timer(interval: .seconds(1)) {
                             await send(.timerTick)
                         }
                     }
@@ -83,11 +74,6 @@ struct CounterFeature {
                 } else {
                     return .cancel(id: CancelID.timer)
                 }
-                
-            case .timerTick:
-                state.count += 1
-                state.fact = nil
-                return .none
             }
         }
     }
